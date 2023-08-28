3940cbc2aa8e50deeab8c8861a236bad5be90571
autonomuscoder <mahiabirgit@tutanota.com>
2023-08-28 12:07:10 +0600

    some changes to polybar

---
+++ bspwm/autostart.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+brightnessctl set 50%
+~/.config/bspwm/launch.sh
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+nitrogen --restore &
+picom &
+cbatticon &
+dunst &
+xclip &
+volumeicon &
+nm-applet &
+sxhkd -c ~/.config/bspwm/sxhkdrc &
