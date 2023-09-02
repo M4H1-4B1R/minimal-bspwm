c4e2103b0c0b01d9ca8e20e0cc742f83af7cdfe3
autonomuscoder <mahiabirgit@tutanota.com>
2023-09-02 17:54:39 +0600

    fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,9 +4,11 @@ end
 
 ### Aliases ###
 
-    alias neofetch='neofetch --ascii_distro arch'
     alias ls='exa --icons'
     alias ll='exa -al --icons'
+		alias snv='sudo -E -s nvim'
+		alias cat='bat --theme Nord'
+### Debian ###
     alias install='sudo apt install --no-install-recommends'
     alias update='sudo apt update'
     alias upgrade='sudo apt update && sudo apt upgrade'
@@ -15,6 +17,17 @@ end
     alias remove='sudo apt remove'
     alias purge='sudo apt purge'
     alias autoremove='sudo apt autoremove'
+### Arch ###
+	  alias pac='sudo pacman -S'
+		alias pacup='sudo pacman -Syu'
+		alias pacrm='sudo pacman -Rns'
+		alias pacfind='sudo pacman -Ss'
+		alias paclist='sudo pacman -Qq'
+		alias orphan='sudo pacman -Qtd'
+		alias foreign='sudo pacman -Qm'
 
     ### Starship ###
      starship init fish | source
+
+ set -U fish_greeting
+
